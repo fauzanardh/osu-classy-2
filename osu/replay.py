8d11c22c43d4ea560a3643191d1b31412b03bb29
Fauzan Ardhana <fauzanardh91@gmail.com>
2023-02-16 16:50:36 +0700

    remove unused stuff

--- osu/replay.py
+++ osu/replay.py
@@ -15,41 +15,6 @@ class GameMode(Enum):
     MANIA = 3
 
 
-class Mod(IntFlag):
-    NoMod = 0
-    NoFail = 1 << 0
-    Easy = 1 << 1
-    TouchDevice = 1 << 2
-    Hidden = 1 << 3
-    HardRock = 1 << 4
-    SuddenDeath = 1 << 5
-    DoubleTime = 1 << 6
-    Relax = 1 << 7
-    HalfTime = 1 << 8
-    Nightcore = 1 << 9
-    Flashlight = 1 << 10
-    Autoplay = 1 << 11
-    SpunOut = 1 << 12
-    Autopilot = 1 << 13
-    Perfect = 1 << 14
-    Key4 = 1 << 15
-    Key5 = 1 << 16
-    Key6 = 1 << 17
-    Key7 = 1 << 18
-    Key8 = 1 << 19
-    FadeIn = 1 << 20
-    Random = 1 << 21
-    Cinema = 1 << 22
-    Target = 1 << 23
-    Key9 = 1 << 24
-    KeyCoop = 1 << 25
-    Key1 = 1 << 26
-    Key3 = 1 << 27
-    Key2 = 1 << 28
-    ScoreV2 = 1 << 29
-    Mirror = 1 << 30
-
-
 class Key(IntFlag):
     M1 = 1 << 0
     M2 = 1 << 1
@@ -162,34 +127,41 @@ class _unpacker:
 
 
 class Replay:
-    def __init__(self, replay_path: str):
+    def __init__(self, replay_path: str, to_np: bool = True):
         self._unpacker = _unpacker(Path(replay_path).read_bytes())
 
-        self._mode = GameMode(self._unpacker.unpack_once("b"))
-        if self._mode != GameMode.STD:
+        # Only store the replay data we need
+        if GameMode(self._unpacker.unpack_once("b")) != GameMode.STD:
             raise ValueError("Only std replays are supported")
+        self._unpacker.unpack_once("i")  # game_version
+        self._unpacker.unpack_string()  # beatmap_hash
+        self._unpacker.unpack_string()  # username
+        self._unpacker.unpack_string()  # replay_hash
+        self._unpacker.unpack_once("h")  # count_300
+        self._unpacker.unpack_once("h")  # count_100
+        self._unpacker.unpack_once("h")  # count_50
+        self._unpacker.unpack_once("h")  # count_geki
+        self._unpacker.unpack_once("h")  # count_katu
+        self._unpacker.unpack_once("h")  # count_miss
+        self._unpacker.unpack_once("i")  # score
+        self._unpacker.unpack_once("h")  # max_combo
+        self._unpacker.unpack_once("?")  # perfect
+        self._unpacker.unpack_once("i")  # mods
+        self._unpacker.unpack_life_bar()  # life_bar
+        self._unpacker.unpack_timestamp()  # timestamp
+        (
+            _,
+            self._replay_data,
+        ) = self._unpacker.unpack_replay_data()  # rng_seed, replay_data
+        self._unpacker.unpack_replay_id()  # replay_id
 
-        self._game_version = self._unpacker.unpack_once("i")
-        self._beatmap_hash = self._unpacker.unpack_string()
-        self._username = self._unpacker.unpack_string()
-        self._replay_hash = self._unpacker.unpack_string()
-        self._count_300 = self._unpacker.unpack_once("h")
-        self._count_100 = self._unpacker.unpack_once("h")
-        self._count_50 = self._unpacker.unpack_once("h")
-        self._count_geki = self._unpacker.unpack_once("h")
-        self._count_katu = self._unpacker.unpack_once("h")
-        self._count_miss = self._unpacker.unpack_once("h")
-        self._score = self._unpacker.unpack_once("i")
-        self._max_combo = self._unpacker.unpack_once("h")
-        self._perfect = self._unpacker.unpack_once("?")
-        self._mods = Mod(self._unpacker.unpack_once("i"))
-        self._life_bar = self._unpacker.unpack_life_bar()
-        self._timestamp = self._unpacker.unpack_timestamp()
-        self._rng_seed, self._replay_data = self._unpacker.unpack_replay_data()
-        self._replay_data_np = None
-        self._replay_id = self._unpacker.unpack_replay_id()
+        # delete unpacker to free memory
+        del self._unpacker
 
-        self.replay_data_to_np()
+        # convert replay data to numpy array
+        self.to_np = to_np
+        if self.to_np:
+            self.replay_data_to_np()
 
     def replay_data_to_np(self):
         t = 0
@@ -201,7 +173,7 @@ class Replay:
             arr[i, 1] = event.x
             arr[i, 2] = event.y
         # sort by time
-        self._replay_data_np = arr[arr[:, 0].argsort()]
+        self._replay_data = arr[arr[:, 0].argsort()]
 
     def cursor(self, t):
         """
@@ -209,17 +181,19 @@ class Replay:
         return cursor position + time since last click at time t (ms)
         """
 
+        assert self.to_np, "Replay data must be converted to numpy array"
+
         # find closest event before t
-        idx = np.searchsorted(self._replay_data_np[:, 0], t, side="right") - 1
+        idx = np.searchsorted(self._replay_data[:, 0], t, side="right") - 1
         if idx < 0:
             raise ValueError("t is before first event")
 
         # if t is after last event, return last event
-        if idx == len(self._replay_data_np) - 1:
-            return (self._replay_data_np[idx, 1], self._replay_data_np[idx, 2]), 0
+        if idx == len(self._replay_data) - 1:
+            return (self._replay_data[idx, 1], self._replay_data[idx, 2]), 0
 
         # interpolate between events
-        t0, x0, y0 = self._replay_data_np[idx]
-        t1, x1, y1 = self._replay_data_np[idx + 1]
+        t0, x0, y0 = self._replay_data[idx]
+        t1, x1, y1 = self._replay_data[idx + 1]
         alpha = (t - t0) / (t1 - t0)
         return (x0 + alpha * (x1 - x0), y0 + alpha * (y1 - y0)), t1 - t
