236e89ecbae429a1d9ed0aaf9f9de42f9fc740c4
Fauzan Ardhana <fauzanardh91@gmail.com>
2023-02-18 14:07:05 +0700

    update vqvae architecture

--- osu_classy/model/vqvae.py
+++ osu_classy/model/vqvae.py
@@ -1,6 +1,6 @@
 import torch
 from torch import nn
-from torch.autograd import Function
+from torch.nn import functional as F
 
 
 def init_weights(m):
@@ -13,199 +13,303 @@ def init_weights(m):
             print(f"Skipping weights init for {class_name}")
 
 
-class VectorQuantization(Function):
-    @staticmethod
-    def forward(ctx, inputs, codebook):
-        with torch.no_grad():
-            emb_size = codebook.size(1)
+class VectorQuantizer(nn.Module):
+    def __init__(
+        self,
+        n_emb,
+        emb_dim,
+        beta,
+        unknown_index="random",
+        legacy=True,
+    ):
+        super().__init__()
 
-            inputs_flatten = inputs.view(-1, emb_size)
+        self.n_emb = n_emb
+        self.emb_dim = emb_dim
+        self.beta = beta
+        self.unknown_index = unknown_index
+        self.legacy = legacy
 
-            distances = torch.addmm(
-                torch.sum(codebook**2, dim=1)
-                + torch.sum(inputs_flatten**2, dim=1, keepdim=True),
-                inputs_flatten,
-                codebook.t(),
-                alpha=-2.0,
-                beta=1.0,
+        self.embedding = nn.Embedding(self.n_emb, self.emb_dim)
+        self.embedding.weight.data.uniform_(-1 / self.n_emb, 1 / self.n_emb)
+
+        self.re_embed = n_emb
+
+    def unmap_to_all(self, inds):
+        ishape = inds.shape
+        assert len(ishape) > 1, "Expected at least 2 dimensions"
+        inds = inds.view(ishape[0], -1)
+        used = self.used.to(inds)
+        if self.re_embed > self.used.shape[0]:
+            inds[inds >= self.used.shape[0]] = 0
+        back = torch.gather(used[None, :][inds.shape[0] * [0], :], 1, inds)
+        return back.view(ishape)
+
+    def forward(self, z):
+        # BCL -> BLC
+        z = z.permute(0, 2, 1).contiguous()
+        z_flatten = z.view(-1, self.emb_dim)
+
+        distances = torch.addmm(
+            (
+                torch.sum(z_flatten**2, dim=1, keepdim=True)
+                + torch.sum(self.embedding.weight**2, dim=1)
+            ),
+            z_flatten,
+            self.embedding.weight.t(),
+            alpha=-2.0,
+            beta=1.0,
+        )
+
+        min_encoding_indices = torch.argmin(distances, dim=-1)
+        z_q = self.embedding(min_encoding_indices).view(z.shape)
+        perplexity = None
+        min_encodings = None
+
+        if not self.legacy:
+            loss = self.beta * torch.mean((z_q.detach() - z) ** 2) + torch.mean(
+                (z_q - z.detach()) ** 2
+            )
+        else:
+            loss = torch.mean((z_q.detach() - z) ** 2) + self.beta * torch.mean(
+                (z_q - z.detach()) ** 2
             )
 
-            _, indices_flatten = torch.min(distances, dim=1)
-            indices = indices_flatten.view(*inputs.size()[:-1])
+        z_q = z + (z_q - z).detach()
 
-            ctx.mark_non_differentiable(indices)
-
-        return indices
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        raise RuntimeError(
-            "Trying to call `.grad()` on graph containing "
-            "`VectorQuantization`. The function `VectorQuantization` "
-            "is not differentiable. Use `VectorQuantizationStraightThrough` "
-            "if you want a straight-through estimator of the gradient."
-        )
-
-
-class VectorQuantizationStraightThrough(Function):
-    @staticmethod
-    def forward(ctx, inputs, codebook):
-        indices = VectorQuantization.apply(inputs, codebook)
-        indices_flatten = indices.view(-1)
-
-        ctx.save_for_backward(indices_flatten, codebook)
-        ctx.mark_non_differentiable(indices)
-        codes_flatten = torch.index_select(codebook, dim=0, index=indices_flatten)
-
-        codes = codes_flatten.view_as(inputs)
-        return codes, indices_flatten
-
-    @staticmethod
-    def backward(ctx, grad_output, grad_indices):
-        grad_inputs, grad_codebook = None, None
-
-        if ctx.needs_input_grad[0]:
-            grad_inputs = grad_output.clone()
-
-        if ctx.needs_input_grad[1]:
-            indices_flatten, codebook = ctx.saved_tensors
-            emb_size = codebook.size(1)
-
-            grad_output_flatten = grad_output.contiguous().view(-1, emb_size)
-
-            grad_codebook = torch.zeros_like(codebook)
-            grad_codebook.index_add_(0, indices_flatten, grad_output_flatten)
-
-        return grad_inputs, grad_codebook
-
-
-class VQEmbedding(nn.Module):
-    def __init__(self, n_emb, emb_dim):
-        super().__init__()
-
-        self.embedding = nn.Embedding(n_emb, emb_dim)
-        self.embedding.weight.data.uniform_(-1 / n_emb, 1 / n_emb)
-
-    def forward(self, z_e_x):
-        # BCL -> BLC
-        z_e_x_ = z_e_x.permute(0, 2, 1).contiguous()
-        latents = VectorQuantization.apply(z_e_x_, self.embedding.weight)
-        return latents
-
-    def straight_through(self, z_e_x):
-        # BCL -> BLC
-        z_e_x_ = z_e_x.permute(0, 2, 1).contiguous()
-
-        z_q_x_, indices = VectorQuantizationStraightThrough.apply(
-            z_e_x_, self.embedding.weight.detach()
-        )
         # BLC -> BCL
-        z_q_x = z_q_x_.permute(0, 2, 1).contiguous()
+        z_q = z_q.permute(0, 2, 1).contiguous()
 
-        z_q_x_bar_flatten = torch.index_select(
-            self.embedding.weight, dim=0, index=indices
-        )
-        z_q_x_bar_ = z_q_x_bar_flatten.view_as(z_e_x_)
-        # BLC -> BCL
-        z_q_x_bar = z_q_x_bar_.permute(0, 2, 1).contiguous()
+        return z_q, loss, (perplexity, min_encodings, min_encoding_indices)
 
-        return z_q_x, z_q_x_bar
+    def get_codebook_entry(self, indices, shape):
+        z_q = self.embedding(indices)
+
+        if shape is not None:
+            z_q = z_q.view(shape)
+            z_q = z_q.permute(0, 2, 1).contiguous()
+
+        return z_q
 
 
-class ConvNextBlock(nn.Module):
-    def __init__(self, h_dim, mult=2, groups=1):
+class Upsample(nn.Module):
+    def __init__(self, in_channels):
         super().__init__()
-
-        self.ds_conv = nn.Conv1d(
-            h_dim, h_dim, 7, padding=3, groups=h_dim, padding_mode="reflect"
-        )
-        self.net = nn.Sequential(
-            nn.GroupNorm(1, h_dim),
-            nn.Conv1d(
-                h_dim,
-                h_dim * mult,
-                7,
-                stride=1,
-                padding=3,
-                padding_mode="reflect",
-                groups=groups,
-            ),
-            nn.SiLU(),
-            nn.GroupNorm(1, h_dim * mult),
-            nn.Conv1d(
-                h_dim * mult,
-                h_dim,
-                7,
-                stride=1,
-                padding=3,
-                padding_mode="reflect",
-                groups=groups,
-            ),
-        )
-        self.apply(init_weights)
+        self.conv = torch.nn.Conv1d(in_channels, in_channels, 7, padding=3)
 
     def forward(self, x):
-        h = self.ds_conv(x)
-        h = self.net(h)
+        x = F.interpolate(x, scale_factor=2.0, mode="linear")
+        x = self.conv(x)
+        return x
+
+
+class Downsample(nn.Module):
+    def __init__(self, in_channels):
+        super().__init__()
+        self.conv = torch.nn.Conv1d(in_channels, in_channels, 7, stride=2, padding=3)
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+
+class ResnetBlock(nn.Module):
+    def __init__(self, in_dim, out_dim, dropout):
+        super().__init__()
+
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+
+        self.norm1 = nn.GroupNorm(1, in_dim)
+        self.conv1 = nn.Conv1d(in_dim, out_dim, 7, padding=3)
+
+        self.norm2 = nn.GroupNorm(1, out_dim)
+        self.dropout = nn.Dropout(dropout)
+        self.conv2 = nn.Conv1d(out_dim, out_dim, 7, padding=3)
+
+        if in_dim != out_dim:
+            self.shortcut = nn.Conv1d(in_dim, out_dim, 1)
+
+    def forward(self, x):
+        h = x
+        h = self.norm1(h)
+        h = F.silu(h)
+        h = self.conv1(h)
+
+        h = self.norm2(h)
+        h = F.silu(h)
+        h = self.dropout(h)
+        h = self.conv2(h)
+
+        if self.in_dim != self.out_dim:
+            x = self.shortcut(x)
+
+        return x + h
+
+
+class Encoder(nn.Module):
+    def __init__(
+        self,
+        in_dim,
+        h_dim,
+        z_dim,
+        double_z=True,
+        dim_mult=(1, 2, 4, 8),
+        num_res_blocks=2,
+        dropout=0.5,
+    ):
+        super().__init__()
+
+        in_dim_mult = (1,) + tuple(dim_mult)
+        self.init_conv = nn.Conv1d(in_dim, h_dim, 7, padding=3)
+
+        # down
+        self.down = nn.ModuleList()
+        for i in range(len(dim_mult)):
+            block = nn.ModuleList()
+            block_in_dim = h_dim * in_dim_mult[i]
+            block_out_dim = h_dim * dim_mult[i]
+            for _ in range(num_res_blocks):
+                block.append(ResnetBlock(block_in_dim, block_out_dim, dropout))
+                block_in_dim = block_out_dim
+
+            down = nn.Module()
+            down.block = block
+            if i != len(dim_mult) - 1:
+                down.downsample = Downsample(block_out_dim)
+
+            self.down.append(down)
+
+        # middle
+        self.middle = nn.Sequential(
+            ResnetBlock(block_in_dim, block_in_dim, dropout),
+            ResnetBlock(block_in_dim, block_in_dim, dropout),
+        )
+
+        # end
+        self.norm = nn.GroupNorm(1, block_in_dim)
+        self.conv_out = nn.Conv1d(
+            block_in_dim, 2 * z_dim if double_z else z_dim, 7, padding=3
+        )
+
+    def forward(self, x):
+        # downsample
+        h = self.init_conv(x)
+        for down in self.down:
+            for block in down.block:
+                h = block(h)
+            if hasattr(down, "downsample"):
+                h = down.downsample(h)
+
+        # middle
+        h = self.middle(h)
+
+        # end
+        h = self.norm(h)
+        h = F.silu(h)
+        h = self.conv_out(h)
         return h
 
 
-class ResBlock(nn.Module):
-    def __init__(self, h_dim):
+class Decoder(nn.Module):
+    def __init__(
+        self,
+        in_dim,
+        h_dim,
+        z_dim,
+        double_z=True,
+        dim_mult=(1, 2, 4, 8),
+        num_res_blocks=2,
+        dropout=0.5,
+    ):
         super().__init__()
 
-        self.block = nn.Sequential(
-            nn.ReLU(),
-            nn.Conv1d(h_dim, h_dim, 7, padding=3, padding_mode="reflect"),
-            nn.BatchNorm1d(h_dim),
-            nn.ReLU(),
-            nn.Conv1d(h_dim, h_dim, 7, padding=3, padding_mode="reflect"),
-            nn.BatchNorm1d(h_dim),
+        dim_mult = tuple(reversed(dim_mult))
+        in_dim_mult = tuple(reversed((1,) + tuple(dim_mult)))
+        block_in_dim = h_dim * in_dim_mult[0]
+
+        self.init_conv = nn.Conv1d(
+            z_dim * 2 if double_z else z_dim, block_in_dim, 7, padding=3
         )
 
+        # middle
+        self.middle = nn.Sequential(
+            ResnetBlock(block_in_dim, block_in_dim, dropout),
+            ResnetBlock(block_in_dim, block_in_dim, dropout),
+        )
+
+        # up
+        self.up = nn.ModuleList()
+        for i in range(len(dim_mult)):
+            block = nn.ModuleList()
+            block_out_dim = h_dim * dim_mult[i]
+            for _ in range(num_res_blocks):
+                block.append(ResnetBlock(block_in_dim, block_out_dim, dropout))
+                block_in_dim = block_out_dim
+
+            up = nn.Module()
+            up.block = block
+            if i != 0:
+                up.upsample = Upsample(block_out_dim)
+
+            self.up.append(up)
+
+        # end
+        self.norm = nn.GroupNorm(1, block_in_dim)
+        self.conv_out = nn.Conv1d(block_in_dim, in_dim, 7, padding=3)
+
     def forward(self, x):
-        return x + self.block(x)
+        # upsample
+        h = self.init_conv(x)
+
+        # middle
+        h = self.middle(h)
+
+        # upsample
+        for up in self.up:
+            for block in up.block:
+                h = block(h)
+            if hasattr(up, "upsample"):
+                h = up.upsample(h)
+
+        # end
+        h = self.norm(h)
+        h = F.silu(h)
+        h = self.conv_out(h)
+        h = torch.sigmoid(h)
+        return h
 
 
 class VQVAE(nn.Module):
-    def __init__(self, in_dim, h_dim, out_dim, n_emb):
+    def __init__(self, in_dim, h_dim, z_dim, n_emb, emb_dim, beta=0.25, double_z=True):
         super().__init__()
 
-        self.encoder = nn.Sequential(
-            nn.Conv1d(in_dim, h_dim, 7, stride=2, padding=3),
-            nn.BatchNorm1d(h_dim),
-            nn.ReLU(),
-            nn.Conv1d(h_dim, h_dim, 7, stride=2, padding=3),
-            ConvNextBlock(h_dim),
-            ConvNextBlock(h_dim),
-        )
-        self.codebook = VQEmbedding(n_emb, h_dim)
-        self.decoder = nn.Sequential(
-            ConvNextBlock(h_dim),
-            ConvNextBlock(h_dim),
-            nn.ReLU(),
-            nn.ConvTranspose1d(h_dim, h_dim, 4, stride=2, padding=1),
-            nn.BatchNorm1d(h_dim),
-            nn.ReLU(),
-            nn.ConvTranspose1d(h_dim, out_dim, 4, stride=2, padding=1),
-            nn.Sigmoid(),
-        )
-        self.apply(init_weights)
+        self.encoder = Encoder(in_dim, h_dim, z_dim, double_z)
+        self.decoder = Decoder(in_dim, h_dim, z_dim, double_z)
+        self.quantize = VectorQuantizer(n_emb, emb_dim, beta=beta)
+        self.quant_conv = nn.Conv1d(z_dim * 2 if double_z else z_dim, emb_dim, 1)
+        self.post_quant_conv = nn.Conv1d(emb_dim, z_dim * 2 if double_z else z_dim, 1)
 
-    def encode(self, x, a):
-        x = torch.cat([x, a], dim=1)
-        z_e_x = self.encoder(x)
-        latents = self.codebook(z_e_x)
-        return latents
+    def encode(self, x):
+        h = self.encoder(x)
+        h = self.quant_conv(h)
+        return self.quantize(h)
 
-    def decode(self, latents):
-        z_q_x = self.codebook.embedding(latents).permute(0, 2, 1)
-        x_tilde = self.decoder(z_q_x)
-        return x_tilde
+    def encode_to_prequant(self, x):
+        h = self.encoder(x)
+        return self.quant_conv(h)
 
-    def forward(self, x, a):
-        x = torch.cat([x, a], dim=1)
-        z_e_x = self.encoder(x)
-        z_q_x_st, z_q_x = self.codebook.straight_through(z_e_x)
-        x_tilde = self.decoder(z_q_x_st)
-        return x_tilde, z_e_x, z_q_x
+    def decode(self, quantized):
+        quantized = self.post_quant_conv(quantized)
+        return self.decoder(quantized)
+
+    def decode_code(self, codebook):
+        quantized_codebook = self.post_quant_conv(codebook)
+        return self.decoder(quantized_codebook)
+
+    def forward(self, x, return_pred_indices=False):
+        quantized, loss, (_, _, ind) = self.encode(x)
+        decoded = self.decode(quantized)
+        if return_pred_indices:
+            return decoded, loss, ind
+        return decoded, loss
