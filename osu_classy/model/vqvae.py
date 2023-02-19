1e7622a7fcc1ce905990394eb6bedd9cf64ec4f9
Fauzan Ardhana <fauzanardh91@gmail.com>
2023-02-19 19:43:21 +0700

    simplify vq, and attention

--- osu_classy/model/vqvae.py
+++ osu_classy/model/vqvae.py
@@ -19,46 +19,27 @@ class VectorQuantizer(nn.Module):
         n_emb,
         emb_dim,
         beta,
-        unknown_index="random",
-        legacy=True,
     ):
         super().__init__()
 
         self.n_emb = n_emb
         self.emb_dim = emb_dim
         self.beta = beta
-        self.unknown_index = unknown_index
-        self.legacy = legacy
 
         self.embedding = nn.Embedding(self.n_emb, self.emb_dim)
         self.embedding.weight.data.uniform_(-1 / self.n_emb, 1 / self.n_emb)
 
         self.re_embed = n_emb
 
-    def unmap_to_all(self, inds):
-        ishape = inds.shape
-        assert len(ishape) > 1, "Expected at least 2 dimensions"
-        inds = inds.view(ishape[0], -1)
-        used = self.used.to(inds)
-        if self.re_embed > self.used.shape[0]:
-            inds[inds >= self.used.shape[0]] = 0
-        back = torch.gather(used[None, :][inds.shape[0] * [0], :], 1, inds)
-        return back.view(ishape)
-
     def forward(self, z):
         # BCL -> BLC
         z = z.permute(0, 2, 1).contiguous()
         z_flatten = z.view(-1, self.emb_dim)
 
-        distances = torch.addmm(
-            (
-                torch.sum(z_flatten**2, dim=1, keepdim=True)
-                + torch.sum(self.embedding.weight**2, dim=1)
-            ),
-            z_flatten,
-            self.embedding.weight.t(),
-            alpha=-2.0,
-            beta=1.0,
+        distances = (
+            torch.sum(z_flatten**2, dim=1, keepdim=True)
+            + torch.sum(self.embedding.weight**2, dim=1)
+            - 2 * torch.einsum("b d, d n -> b n", z_flatten, self.embedding.weight.t())
         )
 
         min_encoding_indices = torch.argmin(distances, dim=-1)
@@ -66,14 +47,9 @@ class VectorQuantizer(nn.Module):
         perplexity = None
         min_encodings = None
 
-        if not self.legacy:
-            loss = self.beta * torch.mean((z_q.detach() - z) ** 2) + torch.mean(
-                (z_q - z.detach()) ** 2
-            )
-        else:
-            loss = torch.mean((z_q.detach() - z) ** 2) + self.beta * torch.mean(
-                (z_q - z.detach()) ** 2
-            )
+        loss = self.beta * torch.mean((z_q.detach() - z) ** 2) + torch.mean(
+            (z_q - z.detach()) ** 2
+        )
 
         z_q = z + (z_q - z).detach()
 
@@ -95,10 +71,11 @@ class VectorQuantizer(nn.Module):
 class Upsample(nn.Module):
     def __init__(self, in_channels):
         super().__init__()
-        self.conv = torch.nn.Conv1d(in_channels, in_channels, 7, padding=3)
+        self.conv = torch.nn.ConvTranspose1d(
+            in_channels, in_channels, 4, stride=2, padding=1
+        )
 
     def forward(self, x):
-        x = F.interpolate(x, scale_factor=2.0, mode="linear")
         x = self.conv(x)
         return x
 
@@ -106,7 +83,9 @@ class Upsample(nn.Module):
 class Downsample(nn.Module):
     def __init__(self, in_channels):
         super().__init__()
-        self.conv = torch.nn.Conv1d(in_channels, in_channels, 7, stride=2, padding=3)
+        self.conv = torch.nn.Conv1d(
+            in_channels, in_channels, 4, stride=2, padding=1, padding_mode="reflect"
+        )
 
     def forward(self, x):
         x = self.conv(x)
@@ -114,41 +93,33 @@ class Downsample(nn.Module):
 
 
 class Attention(nn.Module):
-    def __init__(self, in_dim):
+    def __init__(self, in_dim, heads=8, dim_head=32):
         super().__init__()
+        self.scale = dim_head**-0.5
+        self.heads = heads
+        h_dim = dim_head * heads
+        self.dim_head = dim_head
 
-        self.in_dim = in_dim
-        self.norm = nn.GroupNorm(1, in_dim)
+        self.to_qkv = nn.Conv1d(in_dim, h_dim * 3, 1, bias=False)
+        self.to_out = nn.Conv1d(h_dim, in_dim, 1)
 
-        self.q = nn.Conv1d(in_dim, in_dim, 1)
-        self.k = nn.Conv1d(in_dim, in_dim, 1)
-        self.v = nn.Conv1d(in_dim, in_dim, 1)
+    def attn(self, q, k, v):
+        q = q * self.scale
+        sim = torch.einsum("b h d i, b h d j -> b h i j", q, k)
+        sim = sim - sim.amax(dim=-1, keepdim=True).detach()
+        attn = sim.softmax(dim=-1)
 
-        self.proj_out = nn.Conv1d(in_dim, in_dim, 1)
+        out = torch.einsum("b h i j, b h d j -> b h i d", attn, v)
+        # b h l c -> b (h c) l
+        out = out.permute(0, 1, 3, 2).contiguous()
+        out = out.view(out.shape[0], -1, out.shape[-1])
+        return out
 
     def forward(self, x):
-        h_ = x
-        h_ = self.norm(h_)
-        q = self.q(h_)
-        k = self.k(h_)
-        v = self.v(h_)
-
-        # compute attention
-        q = q.permute(0, 2, 1)  # BCL -> BLC
-        v = v.permute(0, 2, 1)  # BCL -> BLC
-
-        attn = torch.bmm(q, k)  # BLL
-        attn = attn / (int(self.in_dim) ** 0.5)
-        attn = F.softmax(attn, dim=-1)
-
-        # apply attention
-        h_ = torch.bmm(attn, v)
-        h_ = h_.permute(0, 2, 1)  # BLC -> BCL
-
-        h_ = self.proj_out(h_)
-        h_ = h_ + x
-
-        return h_
+        qkv = self.to_qkv(x).chunk(3, dim=1)
+        out = self.attn(*(t.unflatten(1, (self.heads, -1)) for t in qkv))
+        out = self.to_out(out)
+        return out
 
 
 class ResnetBlock(nn.Module):
@@ -165,7 +136,7 @@ class ResnetBlock(nn.Module):
         self.dropout = nn.Dropout(dropout)
         self.conv2 = nn.Conv1d(out_dim, out_dim, 7, padding=3)
 
-        if in_dim != out_dim:
+        if self.in_dim != self.out_dim:
             self.shortcut = nn.Conv1d(in_dim, out_dim, 1)
 
     def forward(self, x):
@@ -185,21 +156,56 @@ class ResnetBlock(nn.Module):
         return x + h
 
 
+class ConvNextBlock(nn.Module):
+    def __init__(self, in_dim, out_dim, dropout, mult=2):
+        super().__init__()
+
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+
+        self.ds_conv = nn.Conv1d(
+            in_dim, in_dim, 7, padding=3, groups=in_dim, padding_mode="reflect"
+        )
+        self.net = nn.Sequential(
+            nn.GroupNorm(1, in_dim),
+            nn.Conv1d(in_dim, out_dim * mult, 7, padding=3, padding_mode="reflect"),
+            nn.SiLU(),
+            nn.Dropout(dropout),
+            nn.GroupNorm(1, out_dim * mult),
+            nn.Conv1d(out_dim * mult, out_dim, 7, padding=3, padding_mode="reflect"),
+        )
+        if self.in_dim != self.out_dim:
+            self.shortcut = nn.Conv1d(in_dim, out_dim, 1)
+
+    def forward(self, x):
+        h = self.ds_conv(x)
+        h = self.net(h)
+
+        if self.in_dim != self.out_dim:
+            x = self.shortcut(x)
+
+        return x + h
+
+
 class Encoder(nn.Module):
     def __init__(
         self,
         in_dim,
         h_dim,
         z_dim,
-        double_z=True,
         dim_mult=(1, 2, 4, 8),
+        use_conv_next=False,
         num_res_blocks=2,
+        attn_heads=8,
+        attn_dim_head=32,
         dropout=0.5,
     ):
         super().__init__()
 
         in_dim_mult = (1,) + tuple(dim_mult)
         self.init_conv = nn.Conv1d(in_dim, h_dim, 7, padding=3)
+
+        res_block = ConvNextBlock if use_conv_next else ResnetBlock
 
         # down
         self.down = nn.ModuleList()
@@ -208,12 +214,12 @@ class Encoder(nn.Module):
             block_in_dim = h_dim * in_dim_mult[i]
             block_out_dim = h_dim * dim_mult[i]
             for _ in range(num_res_blocks):
-                block.append(ResnetBlock(block_in_dim, block_out_dim, dropout))
+                block.append(res_block(block_in_dim, block_out_dim, dropout))
                 block_in_dim = block_out_dim
 
             down = nn.Module()
             down.block = block
-            down.attn = Attention(block_out_dim)
+            down.attn = Attention(block_out_dim, attn_heads, attn_dim_head)
 
             if i != len(dim_mult) - 1:
                 down.downsample = Downsample(block_out_dim)
@@ -222,16 +228,14 @@ class Encoder(nn.Module):
 
         # middle
         self.middle = nn.Sequential(
-            ResnetBlock(block_in_dim, block_in_dim, dropout),
-            Attention(block_in_dim),
-            ResnetBlock(block_in_dim, block_in_dim, dropout),
+            res_block(block_in_dim, block_in_dim, dropout),
+            Attention(block_in_dim, attn_heads, attn_dim_head),
+            res_block(block_in_dim, block_in_dim, dropout),
         )
 
         # end
         self.norm = nn.GroupNorm(1, block_in_dim)
-        self.conv_out = nn.Conv1d(
-            block_in_dim, 2 * z_dim if double_z else z_dim, 7, padding=3
-        )
+        self.conv_out = nn.Conv1d(block_in_dim, z_dim, 7, padding=3)
 
     def forward(self, x):
         # downsample
@@ -259,9 +263,11 @@ class Decoder(nn.Module):
         in_dim,
         h_dim,
         z_dim,
-        double_z=True,
         dim_mult=(1, 2, 4, 8),
+        use_conv_next=False,
         num_res_blocks=2,
+        attn_heads=8,
+        attn_dim_head=32,
         dropout=0.5,
     ):
         super().__init__()
@@ -270,15 +276,15 @@ class Decoder(nn.Module):
         in_dim_mult = tuple(reversed((1,) + tuple(dim_mult)))
         block_in_dim = h_dim * in_dim_mult[0]
 
-        self.init_conv = nn.Conv1d(
-            z_dim * 2 if double_z else z_dim, block_in_dim, 7, padding=3
-        )
+        self.init_conv = nn.Conv1d(z_dim, block_in_dim, 7, padding=3)
+
+        res_block = ConvNextBlock if use_conv_next else ResnetBlock
 
         # middle
         self.middle = nn.Sequential(
-            ResnetBlock(block_in_dim, block_in_dim, dropout),
-            Attention(block_in_dim),
-            ResnetBlock(block_in_dim, block_in_dim, dropout),
+            res_block(block_in_dim, block_in_dim, dropout),
+            Attention(block_in_dim, attn_heads, attn_dim_head),
+            res_block(block_in_dim, block_in_dim, dropout),
         )
 
         # up
@@ -287,12 +293,12 @@ class Decoder(nn.Module):
             block = nn.ModuleList()
             block_out_dim = h_dim * dim_mult[i]
             for _ in range(num_res_blocks):
-                block.append(ResnetBlock(block_in_dim, block_out_dim, dropout))
+                block.append(res_block(block_in_dim, block_out_dim, dropout))
                 block_in_dim = block_out_dim
 
             up = nn.Module()
             up.block = block
-            up.attn = Attention(block_out_dim)
+            up.attn = Attention(block_out_dim, attn_heads, attn_dim_head)
             if i != 0:
                 up.upsample = Upsample(block_out_dim)
 
@@ -325,14 +331,14 @@ class Decoder(nn.Module):
 
 
 class VQVAE(nn.Module):
-    def __init__(self, in_dim, h_dim, z_dim, n_emb, emb_dim, beta=0.25, double_z=True):
+    def __init__(self, in_dim, h_dim, z_dim, n_emb, emb_dim, beta=0.25):
         super().__init__()
 
-        self.encoder = Encoder(in_dim, h_dim, z_dim, double_z)
-        self.decoder = Decoder(in_dim, h_dim, z_dim, double_z)
+        self.encoder = Encoder(in_dim, h_dim, z_dim)
+        self.decoder = Decoder(in_dim, h_dim, z_dim)
         self.quantize = VectorQuantizer(n_emb, emb_dim, beta=beta)
-        self.quant_conv = nn.Conv1d(z_dim * 2 if double_z else z_dim, emb_dim, 1)
-        self.post_quant_conv = nn.Conv1d(emb_dim, z_dim * 2 if double_z else z_dim, 1)
+        self.quant_conv = nn.Conv1d(z_dim, emb_dim, 1)
+        self.post_quant_conv = nn.Conv1d(emb_dim, z_dim, 1)
 
     def encode(self, x):
         h = self.encoder(x)
@@ -354,7 +360,7 @@ class VQVAE(nn.Module):
     def forward(self, x, return_pred_indices=False):
         quantized, loss, (_, _, ind) = self.encode(x)
         decoded = self.decode(quantized)
-        decoded = torch.sigmoid(decoded)
+        decoded = torch.tanh(decoded)
         if return_pred_indices:
             return decoded, loss, ind
         return decoded, loss
