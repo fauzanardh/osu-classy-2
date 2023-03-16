39646f2ff302103ab32d9a4c6e2fea34d4332a10
Fauzan Ardhana <fauzanardh91@gmail.com>
2023-03-16 08:30:21 +0700

    change ff methods

--- osu_classy/model/vqvae.py
+++ osu_classy/model/vqvae.py
@@ -6,20 +6,11 @@ from xformers.ops import memory_efficient_attention
 from vector_quantize_pytorch import VectorQuantize
 
 
-class GEGLU(nn.Module):
-    def forward(self, x):
-        x, gate = x.chunk(2, dim=1)
-        return gate * F.gelu(x)
-
-
 def FeedForward(dim, mult=4):
-    inner_dim = int(dim * mult * 2 / 3)
     return nn.Sequential(
-        nn.GroupNorm(1, dim),
-        nn.Conv1d(dim, inner_dim * 2, 1, bias=False),
-        GEGLU(),
-        nn.GroupNorm(1, inner_dim),
-        nn.Conv1d(inner_dim, dim, 1, bias=False),
+        nn.Conv1d(dim, dim * mult, 1),
+        nn.SiLU(),
+        nn.Conv1d(dim * mult, dim, 1),
     )
 
 
